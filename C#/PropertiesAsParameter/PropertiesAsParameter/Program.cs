44c1173a5fe4880674ab18d611aef7da76558a10
thebernardlim <ber9000@gmail.com>
2017-03-30 22:54:02 +0800

    Updates

--- C#/PropertiesAsParameter/PropertiesAsParameter/Program.cs
+++ C#/PropertiesAsParameter/PropertiesAsParameter/Program.cs
@@ -10,6 +10,18 @@ namespace PropertiesAsParameter
     {
         static void Main(string[] args)
         {
+            //Initial Code - Repetitive. Can be refactored.
+            Footballer f_InitialCode = new Footballer();
+            f_InitialCode.FirstName = "David";
+            Log(f_InitialCode.FirstName);
+            f_InitialCode.LastName = "David";
+            Log(f_InitialCode.LastName);
+            f_InitialCode.ClubName = "Manchester United";
+            Log(f_InitialCode.ClubName);
+            f_InitialCode.Position = "Right Midfielder";
+            Log(f_InitialCode.Position);
+
+            //Refactor attempt #1
             //Passing properties as parameters - Not going to work as strings dont pass by reference unless explicitly stated
             //However it cannot be used as properties are not allowed to be passed as 'ref' or 'out'
             Footballer f_withoutDelegates = new Footballer();
@@ -18,7 +30,8 @@ namespace PropertiesAsParameter
             SetPropertyString(f_withoutDelegates.ClubName, "Manchester United");
             SetPropertyString(f_withoutDelegates.Position, "Right Midfielder");
 
-            //Workaround Solution will be to use Action Delegate as shown below
+            //Refactor attempt #2 - Success
+            //Solution will be to use Action Delegate as shown below
             Footballer f_withDelegates = new Footballer();
 
             SetPropertyHelper("David", s => f_withDelegates.FirstName = s);
@@ -39,6 +52,12 @@ namespace PropertiesAsParameter
         private static void SetPropertyString(string propertyName, string propertyValue)
         {
             propertyName = propertyValue;
+            Log("Value added:" + propertyValue);
+        }
+
+        private static void Log(string logMsg)
+        {
+            //Does logging in real app
         }
     }
 
