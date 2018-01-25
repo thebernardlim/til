d54f45e71caafd644ea8d1fa996d69f6175473c3
thebernardlim <ber9000@gmail.com>
2018-01-25 12:19:15 +0800

    Updated formatting

--- Sitecore/Powershell Scripts/Search-Items-OrderBy-UpdatedTime.ps1
+++ Sitecore/Powershell Scripts/Search-Items-OrderBy-UpdatedTime.ps1
@@ -1,8 +1,9 @@
 ﻿#Purpose: Search all items that have been recently updated in the past day
 #-1 : 1 day, -24 : 24 days etc
 
-cd "/sitecore/media library/Files/xxxx"
-get-childitem -recurse . | `
+cd "\sitecore\Content\Home"
+$items = get-childitem -recurse | `
             Sort $_."__Updated" | `  
-           Where-Object   {  $_."__Updated" -gt (Get-Date).AddDays(-1) }
+           Where-Object   {  $_."__Updated" -gt (Get-Date).AddDays(-20) } 
            
+$items | ForEach-Object { Write-Host ("Name:{0} `t Full Path: {1}" -f $_.Name,$_.Paths.FullPath) }
