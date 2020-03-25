30c2c86b83bde54ee02945b4a7b02f799b6c22be
thebernardlim <ber9000@gmail.com>
2020-03-25 23:30:50 +0800

    ASP.Net Core Sample Project

---
+++ ASP.NET Core/Sample Projects/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public void OnGet()
+        {
+            Message = config["Message"] + " hi";
+            Restaurants = restaurantData.GetAll();
+        }
+
+        public ListModel(IConfiguration config, 
+                        IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+    }
+}
